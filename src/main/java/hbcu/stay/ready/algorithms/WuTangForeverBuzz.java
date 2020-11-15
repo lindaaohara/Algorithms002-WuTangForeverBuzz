eb97d9ca1ef38fbf213b180209e29e3dad4af224
victor840 <victor.ugoanusi@gmail.com>
2020-11-15 16:09:12 -0500

    WuTangForever Code

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -2,7 +2,22 @@ package hbcu.stay.ready.algorithms;
 
 public class WuTangForeverBuzz {
 
-    public String wuTangClan(){
-        return null;
-    }
+    public String wuTangClan() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 1; i <= 15; i++) {
+            if (i % 15 == 0) {
+                stringBuilder.append("WuTang Forever");
+            } else if (i % 5 == 0) {
+                stringBuilder.append("Tang\n");
+            } else if (i % 3 == 0) {
+                stringBuilder.append("Wu\n");
+            } else {
+                stringBuilder.append(i + "\n");
+            }
+        }
+        return stringBuilder.toString();
+        }
+
 }
